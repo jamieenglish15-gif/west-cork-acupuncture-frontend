296bfdff3c127463671437dd0061a0f391f3bde8
jamieenglish15-gif <jamieenglish15@gmail.com>
2026-08-06 06:58:47 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,19 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;"
+          }
+        ]
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;
