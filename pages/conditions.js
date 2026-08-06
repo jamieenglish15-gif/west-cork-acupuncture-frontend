68096a98203ceff66dc4324c64095e9c916bf2f6
jamieenglish15-gif <jamieenglish15@gmail.com>
2026-08-05 20:58:45 -0500

    Update conditions.js

--- pages/conditions.js
+++ pages/conditions.js
@@ -4,7 +4,8 @@ export default function Conditions() {
   const conditions = [
     { title: "Pain & Musculoskeletal", items: ["Back pain", "Neck pain", "Shoulder tension", "Sciatica", "Joint pain", "Sports injuries"] },
     { title: "Women's Health", items: ["PMS & menstrual pain", "Cycle regulation", "Fertility support", "Pregnancy discomfort", "Postpartum recovery"] },
-    { title: "Stress & Mental Health", items: ["Stress", "Anxiety", "Burnout", "Sleep issues", "Emotional imbalance"] },
+    { title: "Men's Health", items: ["Promoting heart health and circulation", "Supporting mental well-being", "Erectile dysfunction", "Overall sexual function"] },
+    { title: "Stress & Mental Health", items: ["Stress", "Anxiety", "Burnout", "Sleep issues", "Emotional imbalance", "Increasing energy and vitality" ] },
     { title: "Digestive Health", items: ["IBS", "Bloating", "Nausea", "Slow digestion", "Gut motility issues"] },
     { title: "Skin & Cosmetic", items: ["Fine lines", "Dull complexion", "Acne", "Facial tension", "Circulation support"] }
   ];
