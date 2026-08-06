0937bd7f64801932c51384356ba7738203dfc7ee
jamieenglish15-gif <jamieenglish15@gmail.com>
2026-08-06 06:51:24 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,27 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-}
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://www.googletagmanager.com https://www.google-analytics.com",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "font-src 'self' https://fonts.gstatic.com",
+              "frame-src https://js.stripe.com https://hooks.stripe.com",
+              "connect-src 'self' https://api.stripe.com https://west-cork-acupuncture-backend-production-366a.up.railway.app https://www.google-analytics.com",
+              "img-src 'self' data: https:",
+            ].join("; ")
+          }
+        ]
+      }
+    ];
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
